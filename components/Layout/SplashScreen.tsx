ab5a512f0b75ea93347932bf6921499439bddc6a
teyim <teyimasobo@gmail.com>
2023-02-24 23:49:23 +0100

    feat: create splashscreen

---
+++ components/Layout/SplashScreen.tsx
@@ -0,0 +1,11 @@
+function LoadingScreen() {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-tl from-indigo-500 to-violet-600 flex justify-center align-middle content-center">
+      <h1 className="font-unbounded font-semibold text-4xl text-orange-300 animate-pulse my-auto">
+        Uba-Votes
+      </h1>
+    </div>
+  );
+}
+
+export default LoadingScreen;
