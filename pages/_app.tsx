f36b7d75ec5a781d64a2630b6cd08bce7ea552bb
teyim <teyimasobo@gmail.com>
2023-02-24 23:52:03 +0100

    refactor(_app.tsx): modify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'HOC/layout';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import SplashScreen from '@components/Layout/SplashScreen';
+import { useStore } from '../utils/storage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +19,23 @@ const queryClient = new QueryClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useStore((state) => ({
+    user: state.user,
+  }));
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (router.pathname === '/campaigns' && !user) {
+      router.replace('/');
+    }
+    setIsLoading(false);
+  }, [user]);
+
+  return isLoading ? (
+    <SplashScreen />
+  ) : (
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
