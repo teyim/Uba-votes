aaf36bfb0cba017f492be0f1b73b536df7b067b3
teyim <teyimasobo@gmail.com>
2023-03-05 14:58:22 +0100

    style(SpashScreen.tsx):modify z-index

--- components/Layout/SplashScreen.tsx
+++ components/Layout/SplashScreen.tsx
@@ -1,7 +1,7 @@
 function SplashScreen() {
   console.log('showing');
   return (
-    <div className="w-screen h-screen bg-gradient-to-tl from-indigo-500 to-violet-600 flex justify-center align-middle content-center absolute z-30">
+    <div className="w-screen h-screen bg-gradient-to-tl from-indigo-500 to-violet-600 flex justify-center align-middle content-center absolute z-50">
       <h1 className="font-unbounded font-semibold text-4xl text-orange-300 animate-pulse my-auto">
         Uba-Votes
       </h1>
