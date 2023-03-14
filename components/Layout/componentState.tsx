2e620448322b4c2702696ddb8c55f9a625563c8b
teyim <teyimasobo@gmail.com>
2023-03-14 17:40:38 +0100

    modify component state

--- components/Layout/componentState.tsx
+++ components/Layout/componentState.tsx
@@ -4,22 +4,25 @@ import React from 'react';
 type ComponentStateProps = {
   isLoading?: boolean;
   isEmpty?: boolean;
+  isError?: boolean;
   currentComponent: string;
 };
 
 function ComponentState({
   isEmpty,
+  isError,
   isLoading,
   currentComponent,
 }: ComponentStateProps) {
   return (
-    <div className="mx-auto">
+    <div className="mx-auto mt-5">
       {isLoading && (
         <h1 className="text-center text-xl">
           Loading {currentComponent}......
         </h1>
       )}
       {isEmpty && <h1>No available {currentComponent}</h1>}
+      {isError && <h1>Failed to fetch {currentComponent}</h1>}
     </div>
   );
 }
